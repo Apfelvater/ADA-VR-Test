5ead6c6fc117764dc6f68afa7125ae42dad09f57
Apfelvater <nicho96@hotmail.de>
2025-08-29 12:08:25 +0200

    password entering implemented

--- Main.py
+++ Main.py
@@ -1,1 +1,50 @@
-# Main
+import time
+
+class _Getch:
+    """Gets a single character from standard input.  Does not echo to the
+screen."""
+    def __init__(self):
+        try:
+            self.impl = _GetchWindows()
+        except ImportError:
+            self.impl = _GetchUnix()
+
+    def __call__(self): return self.impl()
+
+
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+
+class _GetchWindows:
+    def __init__(self):
+        import msvcrt
+
+    def __call__(self):
+        import msvcrt
+        return msvcrt.getch()
+
+
+getch = _Getch()
+
+for i in range(5):
+    EnteredPasswordBytes = []
+    InChar = b''
+    while InChar != b'\r' and InChar != b'\n':
+        EnteredPasswordBytes.append(InChar)
+        InChar = getch()
+        
+    EnteredPasswordString = b''.join(EnteredPasswordBytes).decode()
+    print(f"{i+1}: {EnteredPasswordString}")
